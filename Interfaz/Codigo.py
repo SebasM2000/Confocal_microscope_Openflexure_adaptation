ff494f004616a950ba5854f356ed3fdb7ffffb5d
Sebastian Marin Ruiz <sebastian.marinr@uqvirtual.edu.co>
2023-06-03 22:05:22 -0500

    Segunda prueba

--- Interfaz/Codigo.py
+++ Interfaz/Codigo.py
@@ -16,7 +16,7 @@ root.title("Mi primera ventana")
 root.geometry("900x500")
 
 # Poniendo ícono de la ventana
-root.iconbitmap("D:/SEBASTIAN/UNIVERSIDAD/TRABAJO_GRADO_REPO/Interfaz/TM.ico")
+root.iconbitmap("D:/SEBASTIAN/UNIVERSIDAD/TRABAJO_GRADO_FINAL/Interfaz/TM.ico")
 
 # Limitando las dimensiones de ventana
 root.resizable(0, 0)
@@ -24,20 +24,40 @@ root.resizable(0, 0)
 # Configuraciones color y forma del cursor
 root.config(bg = "#898f8e", cursor = "dot")
 
+nombre = StringVar()
+apellido = StringVar()
+saludo = StringVar()
+
+def saludar():
+    saludo.set("Hola " + nombre.get() + " " + apellido.get())
+
+nombre.set("Escribe aquí tu nombre")
+apellido.set("Escribe aquí tu apellido")
+
 # Etiquetas
-etiqueta = Label(root, text = "Etiqueta")
+etiqueta = Label(root, text = "Etiqueta", bd = 14, bg = "green", font = "Curier 10")
 etiqueta.grid(row = 0, column = 0)
+
+etiqueta2 = Label(root, text = "Escribe aquí tu nombre")
+etiqueta2.place(x = 100, y = 2)
+entrada1 = Entry(root, textvariable = nombre, bd = 5)
+entrada1.place(x = 100, y = 20)
+
+etiqueta3 = Label(root, text = "Escribe aquí tu apellido")
+etiqueta3.place(x = 200, y = 2)
+entrada2 = Entry(root, textvariable = apellido, bd = 5)
+entrada2.place(x = 200, y = 20)
+
+entrada3 = Entry(root, textvariable = saludo, state = "disable")
+entrada3.place(x = 200, y = 200)
 
 # Botones
 boton1 = Button(root, text = "Cerrar", command = root.destroy, bg = "red")
 boton1.grid(row = 2, column = 1)
 
-def imprimir():
-    label1 = Label(root, text = "Imprimiendo algo xd")
-    label1.pack()
 
-boton2 = Button(root, text = "Imprimir", command = imprimir, bg = "blue")
-boton2.place(x = 30, y = 40)
+boton2 = Button(root, text = "Saludar", command = saludar, bg = "blue")
+boton2.place(x = 150, y = 100)
 
 
 
