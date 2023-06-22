d867e747e4c644ec2cf7a2d9204840b6a430a593
SebasM2000 <sebastian.marinr@uqvirtual.edu.co>
2023-06-22 18:33:00 -0500

    Practica clases

--- Control_Arduino/Ctrl_motores.py
+++ Control_Arduino/Ctrl_motores.py
@@ -1,2 +1,34 @@
 import serial
 import time
+
+class carro:
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+        self.arrancado = False
+
+    def arrancar(self):
+        self.arrancado = True
+        print(f"El {self.marca} {self.modelo} ha arrancado")
+
+    def frenar(self):
+        self.arrancado = False
+        print(f"El {self.marca} {self.modelo} ha frenado")
+
+nombre = carro("Renault", "Laguna")
+tesla = carro("Tesla", "Modelo 3")
+
+print(type(nombre))
+print(type(tesla))
+
+print(nombre.marca, nombre.modelo)
+print(tesla.marca, tesla.modelo)
+
+nombre.arrancar()
+tesla.arrancar()
+
+print(nombre.arrancado)
+print(tesla.arrancado)
+
+nombre.frenar()
+tesla.frenar()
