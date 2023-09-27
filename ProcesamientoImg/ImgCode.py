b9132789a124d194a4f791f892c676f2463d37ef
SebasM2000 <sebastian.marinr@uqvirtual.edu.co>
2023-09-27 16:39:31 -0500

    Inicio Procesamiento Img

---
+++ ProcesamientoImg/ImgCode.py
@@ -0,0 +1,21 @@
+# Librerías
+import cv2 as cv
+import numpy as np
+import os
+
+os.environ['QT_QPA_PLATFORM'] = 'xcb'
+
+
+#ruta_abs_img = os.path.dirname(os.path.abspath(__file__))
+#rutaImg = os.path.join(ruta_abs_img, "ProcesamientoImg/IMGPrueba.jpeg")
+rutaImg = "/home/ender3/Sebastian_Marin/ProcesamientoImg/IMGPrueba.jpg"
+
+img = cv.imread(rutaImg)
+img2 = cv.resize(img, (255, 255))
+
+
+
+cv.imshow('Prueba gato', img2)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
